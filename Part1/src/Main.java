53f43be277ea45a86b99968de949ce7b059deb8c
LONEWOLFSW\jtollette <jtollette@lwolf.com>
2017-08-31 10:34:12 -0500

    Added Accept User's Name Method

--- Part1/src/Main.java
+++ Part1/src/Main.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
+public static void HelloWorld() {
+	
+	/*Print "Hello, World" to the screen*/
+	
+	System.out.println("Hello, World");
+	
+	
+}
 
+public static void AcceptUserName() {
+	
+	/* Asks user for name and then stores the name into a variable*/
+	Scanner reader = new Scanner(System.in);
+	System.out.println("Please enter your name: ");
+	String name = reader.next();
+	
+	/* Uses Name variable to respond to user*/
+	System.out.println("Hi, " + name + ". " + "How are you?");
+}
 	public static void main(String[] args) {
 		
-		/*Print "Hello, World" to the screen*/
+	/*Call HelloWorld Method*/
 		
-		System.out.println("Hello, World");
+	HelloWorld();
+	
+	/* Call to AcceptUserName method*/
+	
+	AcceptUserName();
+	
 		
 
 	}
