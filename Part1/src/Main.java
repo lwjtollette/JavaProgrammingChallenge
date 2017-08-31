a5d894ed4396dd5dbb6961b5dea7c4aa685cfd8a
LONEWOLFSW\jtollette <jtollette@lwolf.com>
2017-08-31 11:31:53 -0500

    Added Summation function

--- Part1/src/Main.java
+++ Part1/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+private static Scanner reader;
 public static void HelloWorld() {
 	
 	/*Print "Hello, World" to the screen*/
@@ -12,24 +13,42 @@ public static void HelloWorld() {
 
 public static void AcceptUserName() {
 	
-	/* Asks user for name and then stores the name into a variable*/
-	Scanner reader = new Scanner(System.in);
+	reader = new Scanner(System.in);
 	System.out.println("Please enter your name: ");
 	String name = reader.next();
 	
 	/* Uses Name variable to respond to user*/
 	System.out.println("Hi, " + name + ". " + "How are you?");
 }
+
+public static void Sum() {
+	int sum = 0; // initiating the sum to 0
+	
+	/* Scans for system input to register number from user*/
+	reader = new Scanner(System.in);
+	System.out.println("Please enter an integer of your choice (to find the sum of numbers from 1 to n): ");
+	int number = reader.nextInt(); 
+	sum=number;
+	
+	for (int i = 0; i < number ; i++)
+	{
+		sum = sum +i;
+	}
+	
+	System.out.println("The Sum = " + sum);
+}
 	public static void main(String[] args) {
 		
-	/*Call HelloWorld Method*/
+	/*Prints "Hello, World" to the screen*/
 		
 	HelloWorld();
 	
-	/* Call to AcceptUserName method*/
+	/* Accepts user name input and returns greeting with name*/
 	
 	AcceptUserName();
 	
+	/* Summation of numbers 1 to user input number*/
+	Sum();
 		
 
 	}
